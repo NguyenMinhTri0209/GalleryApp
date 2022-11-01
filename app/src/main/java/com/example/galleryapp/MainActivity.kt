adb6dffd52c6e91d2f03953946d2c686bee35ac9
NguyenMinhTri <minhtriarch84@gmail.com>
2022-11-01 21:59:31 +0700

    first commit

---
+++ app/src/main/java/com/example/galleryapp/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.galleryapp
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.MediaStore.Video.Media
+import android.view.View
+import android.widget.GridLayout
+import android.widget.ProgressBar
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.checkSelfPermission
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
+import java.util.jar.Manifest
+
+class MainActivity : AppCompatActivity() {
+
+    private var imageRecycler:RecyclerView?=null
+    private var progressBar:ProgressBar?=null
+    private var allPicture:ArrayList<Image>?=null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageRecycler=findViewById(R.id.image_recycler)
+        progressBar=findViewById(R.id.recycler_progressbar)
+
+        imageRecycler?.layoutManager=GridLayoutManager(this, 3)
+        imageRecycler?.setHasFixedSize(true)
+
+        if(ContextCompat.checkSelfPermission(
+                this@MainActivity,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+        )!=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                101
+            )
+        }
+
+
+        allPicture = ArrayList()
+        if (allPicture !! .isEmpty()){
+            progressBar?.visibility= View.VISIBLE
+            allPicture = getAllImages()
+            imageRecycler?.adapter=ImageAdapter(this, allPicture!!)
+            progressBar?.visibility=View.GONE
+        }
+
+    }
+
+    private fun getAllImages(): ArrayList<Image>? {
+        val images=ArrayList<Image>()
+
+        val allImageUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+
+        val projection = arrayOf(MediaStore.Images.ImageColumns.DATA, MediaStore.Images.Media.DISPLAY_NAME)
+
+        var cursor = this@MainActivity.contentResolver.query(allImageUri, projection,null,null,null)
+
+        try {
+            cursor!!.moveToFirst()
+            do {
+                val image=Image()
+                image.imagePath = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA))
+                image.imageName = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME))
+                images.add(image)
+            }while (cursor.moveToNext())
+            cursor.close()
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
+        return images
+    }
+}
